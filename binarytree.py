62aa110435ee6eb04377a8e57c6e2c48372bf284
yogomason <66168985+yogomason@users.noreply.github.com>
2021-01-03 00:12:32 +0000

    Finished tree and added the 3 traversals(postorder,inorder and preorder)

--- binarytree.py
+++ binarytree.py
@@ -1,3 +1,5 @@
+global ans
+
 arr = [0,1,2,3,4,5,6]
 
 class Node:
@@ -11,7 +13,46 @@ class Tree:
 
     def __init__(self, arr):
         self.arr = arr
-        self.head = Node(arr[len(arr)//2])
+        self.head = self.createtree(arr)
+    
+    def createtree(self,arr):
+        if arr == []:
+            return 
+        node = Node(arr[len(arr)//2])
+        node.left = self.createtree(arr[:len(arr)//2])
+        node.right = self.createtree(arr[(len(arr)//2)+1:len(arr)])
+        return node
+
+        
+
 
 tree = Tree(arr)
-print(tree.head.val)
+
+ans = []
+def preorder(node):
+    global ans
+    ans.append(node.val)
+    if node.left != None:
+        preorder(node.left)
+    if node.right != None:
+        preorder(node.right)
+
+def inorder(node):
+    global ans
+    if node.left != None:
+        inorder(node.left)
+    ans.append(node.val)
+    if node.right != None:
+        inorder(node.right)
+
+def postorder(node):
+    global ans
+    if node.left != None:
+        postorder(node.left)
+    if node.right != None:
+        postorder(node.right)
+    ans.append(node.val)
+
+
+postorder(tree.head)
+print(ans)
